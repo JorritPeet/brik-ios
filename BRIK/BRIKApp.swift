42474ed3623ac0920e02e9d681d1a3db60885aa0
Jorrit Peet <jorritpeet@MacBookAir.home>
2025-07-28 19:45:43 +0200

    Initial commit for BRIK

---
+++ BRIK/BRIKApp.swift
@@ -0,0 +1,17 @@
+//
+//  BRIKApp.swift
+//  BRIK
+//
+//  Created by Jorrit Peet on 28/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct BRIKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BRIKTests/BRIKTests.swift
@@ -0,0 +1,16 @@
+//
+//  BRIKTests.swift
+//  BRIKTests
+//
+//  Created by Jorrit Peet on 28/07/2025.
+//
+
+import Testing
+
+struct BRIKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
